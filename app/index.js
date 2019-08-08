0ad29fd17e70bc46202a9c251947451cdc19ab6c
Willyan Lin <willyanlin@gmail.com>
2019-08-08 13:23:16 +0200

    add nav with li key

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 // Component
 // State
 // Lifecycle
@@ -9,8 +10,9 @@ import './index.css'
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className='container'>
                 Hello 33!
+                <Popular />
             </div>
         )
     }
